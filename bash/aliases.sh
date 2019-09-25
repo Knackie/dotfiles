4cd542ae4230f0962550aba0e054eaf7b0096790
William <pedroschneider@outlook.com>
2019-09-25 17:12:02 -0300

    #2

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -4,3 +4,6 @@ alias ...='cd ../..'
 alias l='ls -la'
 alias p='python3'
 alias p2='python'
+alias emacs="emacs -nw"
+alias vv="vim"
+alias vi="nvim"
--- bash/path.sh
+++ bash/path.sh
@@ -1,2 +1,10 @@
 PATH="$PATH:/usr/local/bin/tmux"
+
+export PATH="/Users/william/PathStuff:$PATH"
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+export PATH="/usr/local/opt/qt/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+
 export PATH
